cfe6ffb7be9c910858440f3259c46849164e6319
dibyasmitahati <dibyasmita1202@gmail.com>
2025-08-02 04:05:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-EMAIL_ADDRESS = "dibyasmita1202@gmail.com"
-EMAIL_PASSWORD = "mgfjxlguwgmwhrrm"
+#Replace with your Gmail credentials (App Password required)
+EMAIL_ADDRESS = "youremailadrress@gmail.com"
+EMAIL_PASSWORD = "you_app_password_here"
